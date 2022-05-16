1b3ff640247eec590b14bab56cb5674392d72d9a
unknown <mhmmshhzad@gmail.com>
2022-05-15 21:21:11 -0400

    homework

---
+++ src/hw1JavaBasics/Myself.java
@@ -0,0 +1,14 @@
+package hw1JavaBasics;
+
+public class Myself {
+	public static void main(String[] args) {
+		System.out.println("Muhammad Shahzad");
+		System.out.print("Po Box 1089 Selden NY 11784");
+	}
+}
+
+/*
+ * 
+ *
+ */
+
---
+++ src/hw2Javabasics/MyCar.java
@@ -0,0 +1,9 @@
+package hw2Javabasics;
+
+public class MyCar {
+public static void main(String[] args) {
+	System.out.println("My name is: Muhammad Shahzad, +"");
+	System.out.println("");
+	
+}
+}
