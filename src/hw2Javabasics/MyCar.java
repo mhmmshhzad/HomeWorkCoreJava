71acee4b0d52e70cc6e649217b10775ea1e3a261
unknown <mhmmshhzad@gmail.com>
2022-05-16 23:24:38 -0400

    Homework 05/16/22 11:25PM

--- src/hw2Javabasics/MyCar.java
+++ src/hw2Javabasics/MyCar.java
@@ -2,8 +2,8 @@ package hw2Javabasics;
 
 public class MyCar {
 public static void main(String[] args) {
-	System.out.println("My name is: Muhammad Shahzad, +"");
-	System.out.println("");
+	System.out.println("Muhammad Shahzad");
+	
 	
 }
 }
